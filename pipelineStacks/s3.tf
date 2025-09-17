2b85cb7ad03890318186323ad971a689b1f38bbf
Abrorjon77 <110739859+Abrorjon77@users.noreply.github.com>
2025-09-17 03:49:16 -0400

    Update s3.tf
    add backend

--- pipelineStacks/s3.tf
+++ pipelineStacks/s3.tf
@@ -4,5 +4,11 @@ resource "aws_s3_bucket" "example" {
 
   force_destroy = true
 }
-
+terraform {
+  backend "s3" {
+    bucket = "mys3backendstate"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 #add your state file s3 bucket code here
