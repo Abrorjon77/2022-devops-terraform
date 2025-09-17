00f81a2faf696f22fcc32acc850baac283ff98f7
abror <abroor@yahoo.com>
2025-09-17 15:11:34 -0400

    deleting backend s3

--- MODULES/S3/s3.tf
+++ MODULES/S3/s3.tf
@@ -1,17 +1,13 @@
 locals {
   prefix = "ziyotek-${var.bucket_name}"
 }
-terraform {
-  backend "s3" {
-    bucket = "mys3backendstate"
-    key    = "state/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
 
-#resource "aws_s3_bucket" "devops_s3_course_2022_moved_resource" {
- # count = var.create_s3 ? 1 : 0
-  #bucket = "${var.environment}-${local.prefix}-${data.aws_region.current.id}-${count.index}"
+
+data "aws_region" "current" {}
+
+resource "aws_s3_bucket" "devops_s3_course_2022_moved_resource" {
+  count = var.create_s3 ? 1 : 0
+  bucket = "${var.environment}-${local.prefix}-${data.aws_region.current.id}-${count.index}"
 
   versioning {
     enabled = var.versioning
