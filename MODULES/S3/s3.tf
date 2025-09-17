a5db20a361faf2be7b8827a78a62c6b3fb2f70a9
Abrorjon77 <110739859+Abrorjon77@users.noreply.github.com>
2025-09-17 10:41:03 -0400

    Update s3.tf modify

--- MODULES/S3/s3.tf
+++ MODULES/S3/s3.tf
@@ -1,10 +1,16 @@
 locals {
   prefix = "ziyotek-${var.bucket_name}"
 }
+backend "s3" {
+    bucket = var.artifact_bucket
+    key    = "state/terraform.tfstate"
+    region = var.region
+  }
+}
 
-resource "aws_s3_bucket" "devops_s3_course_2022_moved_resource" {
-  count = var.create_s3 ? 1 : 0
-  bucket = "${var.environment}-${local.prefix}-${data.aws_region.current.id}-${count.index}"
+#resource "aws_s3_bucket" "devops_s3_course_2022_moved_resource" {
+ # count = var.create_s3 ? 1 : 0
+  #bucket = "${var.environment}-${local.prefix}-${data.aws_region.current.id}-${count.index}"
 
   versioning {
     enabled = var.versioning
