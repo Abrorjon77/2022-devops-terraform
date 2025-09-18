13be3d0188a2d2262819317604776efdadcc74c6
abror <abroor@yahoo.com>
2025-09-18 12:53:29 -0400

    change cidr of vpc

--- environments/PRODUCTION/vpc.tf
+++ environments/PRODUCTION/vpc.tf
@@ -2,8 +2,8 @@ module "vpc" {
     source = "../MODULES/VPC"
     environment = var.environment
 
-    vpc_cidr_block = "20.10.0.0/16"
-    # subnet_cidr_1 = "20.10.10.0/24"
-    # subnet_cidr_2 = "20.10.20.0/24"
-    subnet_cidr_private = "20.10.30.0/24"
+    vpc_cidr_block = "10.10.0.0/16"
+    # subnet_cidr_1 = "10.10.10.0/24"
+    # subnet_cidr_2 = "10.10.20.0/24"
+    subnet_cidr_private = "10.10.30.0/24"
 }
