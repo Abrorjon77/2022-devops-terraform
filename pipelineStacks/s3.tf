1292cd1639fd2cb9df2f819809a65ee306893472
abror <abroor@yahoo.com>
2025-09-18 11:51:39 -0400

    resource to data s3

--- pipelineStacks/s3.tf
+++ pipelineStacks/s3.tf
@@ -4,12 +4,20 @@
 
   //force_destroy = true
  //}
- resource "aws_s3_bucket" "example" {
-  bucket = "mys3backendstate"
+ //resource "aws_s3_bucket" "example" {
+  //bucket = "mys3backendstate"
   # acl argument removed due to deprecation
-  region = "us-east-1"
+  //region = "us-east-1"
+//}
+data "aws_s3_bucket" "existing" {
+  bucket = "mys3backendstate"
 }
 
+resource "aws_s3_bucket_object" "tfstate" {
+  bucket = data.aws_s3_bucket.existing.id
+  key    = "terraform.tfstate"
+  //source = "path/to/your/local/terraform.tfstate"
+}
 
   
 
