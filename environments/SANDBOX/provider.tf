ad8d77b072701934cc578e6310f12310e180387d
abror <abroor@yahoo.com>
2026-01-30 08:54:12 -0500

    change var

--- environments/SANDBOX/provider.tf
+++ environments/SANDBOX/provider.tf
@@ -2,14 +2,14 @@ provider "aws" {
     region = "us-east-1"
     default_tags {
       tags = {
-        Name = "ziyotek-devops-${var.environment}"
+        Name = var.environment
       }
     }
 }
 
 terraform {
   backend "s3" {
-    bucket = "abror-pipeline-s3-bucket"
+    bucket = "abrorjoncodepipelines3bucket"
     key    = "dags/myfile"
     region = "us-east-1"
   }
